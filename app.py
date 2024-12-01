d564ea9de0c590083fa8d8efc9b66a567ea3bc37
the-atasoy <barisatasoy2917@gmail.com>
2024-12-01 17:17:14 +0300

    done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 import numpy as np
 from model import load_model, preprocess_input
 
@@ -6,35 +6,24 @@ app = Flask(__name__)
 
 model, scaler = load_model()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        cylinders = int(request.form['cylinders'])
-        displacement = float(request.form['displacement'])
-        horsepower = float(request.form['horsepower'])
-        weight = float(request.form['weight'])
-        acceleration = float(request.form['acceleration'])
-        model_year = int(request.form['model_year'])
-        origin = int(request.form['origin'])
+    try:
+        data = request.get_json()
 
-        if (origin == 1):
-            origin_america = 1
-            origin_europa = 0
-            origin_japan = 0
-        elif (origin == 2):
-            origin_europa = 1
-            origin_america = 0
-            origin_japan = 0
-        else:
-            origin_japan = 1
-            origin_america = 0
-            origin_europa = 0
+        cylinders = int(data['cylinders'])
+        displacement = float(data['displacement'])
+        horsepower = float(data['horsepower'])
+        weight = float(data['weight'])
+        acceleration = float(data['acceleration'])
+        model_year = int(data['model_year'])
+        origin = int(data['origin'])
 
-        data = {
+        origin_america = 1 if origin == 1 else 0
+        origin_europa = 1 if origin == 2 else 0
+        origin_japan = 1 if origin == 3 else 0
+
+        input_data = {
             'cylinders': cylinders,
             'displacement': displacement,
             'horsepower': horsepower,
@@ -45,11 +34,18 @@ def predict():
             'origin_europa': origin_europa,
             'origin_japan': origin_japan
         }
-        
-        data_scaled = preprocess_input(data)
+
+        data_scaled = preprocess_input(input_data)
 
         prediction = model.predict(data_scaled)
-        return render_template('index.html', prediction_text=f'Predicted MPG: {prediction[0]:.2f}')
+
+        response = {
+            'predicted_mpg': round(prediction[0], 2)
+        }
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
