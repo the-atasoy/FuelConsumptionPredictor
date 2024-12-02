8a00f96529f81879598f7a0a1c08c59976bcf1d3
Barış Atasoy <batasoy@ford.com.tr>
2024-12-02 16:36:35 +0300

    unnecessary comments

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import pickle
 import pandas as pd
 
-# Function to load the model and scaler
 def load_model():
     with open('lasso_model.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -9,15 +8,12 @@ def load_model():
         scaler = pickle.load(f)
     return model, scaler
 
-# Function to preprocess the input features
 def preprocess_input(data):
     data = pd.DataFrame(data, index=[0])
 
-    # Standardize features
     feature_columns = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model year', 'origin_america', 'origin_europa', 'origin_japan']
     data = data[feature_columns]
 
-    # Load the scaler and transform
     _, scaler = load_model()
     data_scaled = pd.DataFrame(scaler.transform(data))
     return data_scaled
